65f0c832eeeb0f2fcc0a2e2264c54a6f66e57223
saikumartheppa <saikumartheppa06@gmail.comgit config --global user.email saikumartheppa06@gmail.com>
2023-06-24 21:10:25 +0530

    UrlHitCounter Assignment

---
+++ URLHit-Counter/URLHit-Counter/src/main/java/com/saikumar/URLHitCounter/Controller/UrlHitCounterController.java
@@ -0,0 +1,28 @@
+package com.saikumar.URLHitCounter.Controller;
+
+import com.saikumar.URLHitCounter.service.UrlHitService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("api/v1/visitor-count-app")
+public class UrlHitCounterController {
+    @Autowired
+    private UrlHitService urlHitService;
+
+    @GetMapping("count")
+    public Integer getHitCount() {
+        return  urlHitService.getHitCount();
+    }
+
+    @GetMapping("username/{username}/count")
+    public Map<String, Object> getHitCount(@PathVariable String username) {
+        return urlHitService.getHitCount(username);
+    }
+}
